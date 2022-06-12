21e606066e611e026a7a5b43d1ea937ff4a274fc
y.romanchuk <y.romanchuk@bi.zone>
2022-06-12 20:00:00 +0300

    backend: added prefix

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from . import auth, category, companies, good
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 router.include_router(auth.router)
 router.include_router(category.router)
 router.include_router(companies.router)
